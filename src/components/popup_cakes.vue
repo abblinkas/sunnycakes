e623cbf2c9b24c7fb1ca6029a63fc8d79eec1ad9
VictorFagerstrom <43879842+VictorFagerstrom@users.noreply.github.com>
2021-05-25 08:22:04 +0200

    nja

---
+++ src/components/popup_cakes.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- you define your Vue template here -->
+  <q-card class="forum-style row justify-center"> </q-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,69 +1,106 @@
 <template>
   <div class="q-pa-md">
-    <div class="row">
+    <div class="row ">
       <div class="col-3 q-pa-md">
-        <q-card class="my-card">
-          <img src="https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/lemoncheese_cake.jpg">
+        <q-card @click="alert = true" class="my-card no-margin full-height">
+          <img
+            style="height: 70%"
+            src="https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/lemoncheese_cake.jpg"
+          />
+
+          <q-card-section class="q-pa-sm">
+            <div class="text-h6">Our Changing Planet</div>
+            <div class="text-subtitle2">
+              by John Doeonpsdnicnsincidsnciopnspc
+            </div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none"> </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-3 q-pa-md">
+        <q-card class="my-card no-margin full-height">
+          <img
+            style="height: 70%"
+            src="https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/victoria_sponge.jpg"
+          />
 
           <q-card-section>
             <div class="text-h6">Our Changing Planet</div>
             <div class="text-subtitle2">by John Doe</div>
           </q-card-section>
 
-          <q-card-section class="q-pt-none">
-            {{ lorem }}
-          </q-card-section>
+          <q-card-section class="q-pt-none"> </q-card-section>
         </q-card>
       </div>
       <div class="col-3 q-pa-md">
-        <q-card class="my-card">
-          <img src="https://raw.githubusercontent.com/filippella/Dagger-Rx-Database-MVP/master/cakes/victoria_sponge.jpg">
+        <q-card class="my-card no-margin full-height">
+          <img
+            src="https://cdn.quasar.dev/img/mountains.jpg"
+            style="height: 70%"
+          />
 
           <q-card-section>
             <div class="text-h6">Our Changing Planet</div>
             <div class="text-subtitle2">by John Doe</div>
           </q-card-section>
 
-          <q-card-section class="q-pt-none">
-            {{ lorem }}
-          </q-card-section>
+          <q-card-section class="q-pt-none"> </q-card-section>
         </q-card>
       </div>
       <div class="col-3 q-pa-md">
-        <q-card class="my-card">
-          <img src="https://cdn.quasar.dev/img/mountains.jpg">
+        <q-card class="my-card no-margin full-height">
+          <img
+            style="height: 70%"
+            src="https://cdn.quasar.dev/img/mountains.jpg"
+          />
 
           <q-card-section>
             <div class="text-h6">Our Changing Planet</div>
             <div class="text-subtitle2">by John Doe</div>
           </q-card-section>
 
-          <q-card-section class="q-pt-none">
-            {{ lorem }}
-          </q-card-section>
+          <q-card-section class="q-pt-none"> </q-card-section>
         </q-card>
       </div>
       <div class="col-3 q-pa-md">
-        <q-card class="my-card">
-          <img src="https://cdn.quasar.dev/img/mountains.jpg">
+        <q-card class="my-card no-margin full-height">
+          <img
+            style="height: 70%"
+            src="https://cdn.quasar.dev/img/mountains.jpg"
+          />
 
           <q-card-section>
             <div class="text-h6">Our Changing Planet</div>
             <div class="text-subtitle2">by John Doe</div>
           </q-card-section>
 
-          <q-card-section class="q-pt-none">
-            {{ lorem }}
-          </q-card-section>
+          <q-card-section class="q-pt-none"> </q-card-section>
         </q-card>
       </div>
-
     </div>
+    <q-dialog v-model="alert">
+      <q-card class="my-card">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none"> </q-card-section>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  data() {
+    return {
+      alert: false
+    };
+  }
+};
 </script>
